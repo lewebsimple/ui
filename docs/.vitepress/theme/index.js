f4bb272fa3972d8e1c20ab6af5ba131ae954336e
Pascal Martineau <pascal@lewebsimple.ca>
2022-11-07 21:17:59 -0500

    feat: Initial Tailwind CSS config

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,8 @@
+import DefaultTheme from "vitepress/theme";
+import "./tailwind.css";
+
+export default {
+  ...DefaultTheme,
+  // enhanceApp({ app, router, siteData }) {},
+  // setup () {}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./{docs,src}/**/*.{js,ts,vue}"],
+  plugins: [],
+};
