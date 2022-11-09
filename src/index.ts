01ed9d64bc912644c116c9929e3f0ddaee9f894d
Pascal Martineau <pascal@lewebsimple.ca>
2022-11-08 21:22:23 -0500

    feat: Vue 3 plugin as default export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export { default as Alert } from "./components/Alert.vue";
+import { App } from "vue";
+import * as components from "./components";
+
+export default {
+  install: (app: App) => {
+    for (const [key, component] of Object.entries(components)) {
+      app.component(key, component);
+    }
+  },
+};
+
+export * from "./components";
