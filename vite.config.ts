5a16b7f07f429c0c069ba2299226750d8d847df0
Pascal Martineau <pascal@lewebsimple.ca>
2022-11-13 20:56:00 -0500

    fix: resolve "@/*" in Vue components

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import vue from "@vitejs/plugin-vue";
 
 module.exports = defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
