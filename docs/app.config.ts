621f145490df74bfb1795fcd37209d497355b1f4
Pascal Martineau <pascal@lewebsimple.ca>
2024-06-06 12:00:28 -0400

    feat: Documentation package

---
+++ docs/app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  docus: {
+    title: "@lewebsimple/ui",
+    description: "Websimple components library",
+    url: "https://github.com/lewebsimple/ui",
+  },
+});
