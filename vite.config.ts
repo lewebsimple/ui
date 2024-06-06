4903f9ee7b2b70dec0ce9f9644894de613bee395
Pascal Martineau <pascal@lewebsimple.ca>
2024-06-06 10:07:27 -0400

    feat: emit declarations using vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    dts({
+      outDir: "dist/types",
+      staticImport: true,
+    }),
+    vue(),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
