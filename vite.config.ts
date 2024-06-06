f4f1f1ec98bd0c21d4437e36bfb32043ac4d0155
Pascal Martineau <pascal@lewebsimple.ca>
2024-06-06 09:56:04 -0400

    feat: Initial library mode with Alert component

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "@lewebsimple/ui",
+      fileName: "ui",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
